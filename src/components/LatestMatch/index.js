cc4fe80a0892140406d466679e527ae753a3286b
Vishnuvardhan-svv <vishnuvardhan.svv123@gmail.com>
2026-01-11 05:52:51 +0000

    Added Logic for Latest Match and Recent Matches

--- src/components/LatestMatch/index.js
+++ src/components/LatestMatch/index.js
@@ -1,1 +1,38 @@
 // Write your code here
+const Latestmatch = props => {
+  const {latestMatchDetails} = props
+  const {
+    umpires,
+    result,
+    manOfTheMatch,
+    date,
+    venue,
+    competingTeam,
+    competingTeamLogo,
+    firstInnings,
+    secondInnings,
+    matchStatus,
+  } = latestMatchDetails
+  return (
+    <div>
+      <div>
+        <h1>{competingTeam}</h1>
+        <p>{date}</p>
+        <p>{venue}</p>
+        <p>{result}</p>
+      </div>
+      <img src={competingTeamLogo} alt={} />
+      <div>
+        <p>First Innings</p>
+        <p>{firstInnings}</p>
+        <p>Second Innings</p>
+        <p>{secondInnings}</p>
+        <p>Man Of The match</p>
+        <p>{manOfTheMatch}</p>
+        <p>Umpires</p>
+        <p>{umpires}</p>
+      </div>
+    </div>
+  )
+}
+export default Latestmatch
--- src/components/MatchCard/index.js
+++ src/components/MatchCard/index.js
@@ -1,1 +1,14 @@
 // Write your code here
+const MatchCard = props => {
+  const {each} = props
+  const {competingTeamLogo, result, competingTeam, matchStatus} = each
+  return (
+    <li>
+      <img src={competingTeamLogo} alt={`competing team ${competingTeam}`} />
+      <p>{competingTeam}</p>
+      <p>{result}</p>
+      <p>{matchStatus}</p>
+    </li>
+  )
+}
+export default MatchCard
